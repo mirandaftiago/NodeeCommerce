433063e72b14ae6f4a85564e26f290e6ac107ff2
Tiago <miranda.f.tiago@gmail.com>
2021-05-31 12:32:28 +0100

    Adding Pug support

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const rootDir = require('./util/path');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views')
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -15,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404');
 });
 
 app.listen(3000);
